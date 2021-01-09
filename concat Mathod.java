5d28dd46524eab0d74485032e949995bde5794e4
Ankur Rajput <57582813+rajput2804@users.noreply.github.com>
2021-01-09 06:48:53 -0800

    Create concat Mathod.java

---
+++ concat Mathod.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   String firstName = "Ankur";
+	   String lastName  = "Rajput";
+     System.out.println(firstName.concat(lastName));
+		// your code goes here
+	}
+}
